5b8fab877895dee478e989e2ca82c5450ee2cb79
Josh Kropf <josh@slashdev.ca>
2022-04-28 13:35:43 -0400

    add test

---
+++ src/test/java/com/ncc/testing/BaseTest.java
@@ -0,0 +1,10 @@
+package com.ncc.testing;
+
+import org.junit.Before;
+
+public class BaseTest {
+   @Before
+   public void setup() {
+      System.out.println("Hello junit setup");
+   }
+}
---
+++ src/test/java/com/ncc/testing/UnitTest.java
@@ -0,0 +1,12 @@
+package com.ncc.testing;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class UnitTest extends BaseTest {
+   @Test
+   public void testFoo() {
+      assertNotNull("Hello, World!");
+   }
+}
